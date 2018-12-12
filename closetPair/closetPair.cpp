f9b6ecbdeea1080e2f5928220faf6bac51af4085
yuan3675 <yuan3675@gmail.com>
2018-12-12 23:33:56 +0800

    not finish

--- closetPair/closetPair.cpp
+++ closetPair/closetPair.cpp
@@ -2,44 +2,72 @@
 #include <vector>
 #include <algorithm>
 #include <stdio.h>
+#include <stdlib.h>
 #include <climits>
 #include <cmath>
 
 using namespace std;
 
 long minDis = LONG_MAX;
+vector<vector<int>> lookUp;
+vector<vector<vector<int>>> closestPairs;
+
+bool compareX(const vector<int> &a, const vector<int> &b) {
+    return a[0] < b[0];
+}
+
+bool compareY(const vector<int> &a, const vector<int> &b) {
+    return a[1] < b[1];
+}
 
 long getDistance(vector<int> a, vector<int> b) {
     return (long int)(pow((a.at(0) - b.at(0)), 2) + pow((a.at(1) - b.at(1)), 2));
 }
 
-int findMiddleX(vector<int> tmp) {
-    sort(tmp.begin(), tmp.end());
-    return tmp.at(tmp.size()/2);
-}
-
-void findClosetPairs(int num, vector<vector<int>> points) {
-    // not safe, may have error
-    if (num <= 2) { 
-        if (num == 2) {
-            long dis = getDistance(points.at(0), points.at(1));
-            if (dis < minDis) minDis = dis;
+void bruteForce(vector<vector<int>> points) {
+    for (int i = 0; i < points.size(); i++) {
+        for (int j = i+1; j < points.size(); j++) {
+            dis = getDistance(points.at(i), points.at(j));
+            if (dis < minDis) {
+                minDis = dis;
+                closestPairs.clear();
+                vector<vector<int>> closestPair = {points.at(i), points.at(j)};
+                closestPairs.push_back(closestPair);
+            }
+            else if (dis == minDis) {
+                vector<vector<int>> closestPair = {points.at(i), points.at(j)};
+                closestPairs.push_back(closestPair);
+            }
         }
     }
+}
+
+void mergeStrip(vector<vector<int>> points) {
+    sort(points.begin(), points.end(), compareY);
+    for (int i = 0; i <     
+}                
+
+void findClosestPairs(vector<vector<int>> points) {
+    // not safe, may have error
+    if (points.size() <= 3) {
+        bruteForce(points); 
+    }
     else {
-        vector<int> tmp(num, 0);
-        for (int i = 0; i < num; i++) {
-            tmp.at(i) = points.at(i).at(0);
-        }
-        int middle = findMiddleX(tmp);
+        sort(points.begin(), points.end(), compareX);
+        int middle = points.at(points.size()/2).at(0);
         vector<vector<int>> L, R;
-        for (int i = 0; i < num; i++) {
+        for (int i = 0; i < points.size(); i++) {
             if (points.at(i).at(0) <= middle) L.push_back(points.at(i));
             else R.push_back(points.at(i));
         }
-        findClosetPairs(L.size(), L);
-        findClosetPairs(R.size(), R);
+        findClosetPairs(L);
+        findClosetPairs(R);
         // TODO: merge
+        vector<vector<int>> strip;
+        for (int i = 0; i < points.size(); i++) {
+            if ((long int)(abs(points.at(i).at(0))) - middle <= sqrt(minDis)) strip.push_back(points.at(i));
+        }
+	mergeStrip(strip);
     } 
 }
 
@@ -49,6 +77,7 @@ int main() {
     vector<int> tmp(2, 0);
     vector<vector<int>> points;
     points.assign(pointNum, tmp);
+    lookUp.assign(pointNum, tmp);
     
     for (int i = 0; i < pointNum; i++) {
         int x, y;
@@ -56,9 +85,8 @@ int main() {
         scanf("%d", &y);
         points.at(i).at(0) = x;
         points.at(i).at(1) = y;
+        lookUp.at(i).at(0) = x;
+        lookUp.at(i).at(1) = y;
     }
-    sort(points.begin(), points.end(), [](const vector<int> &a, const vector<int> &b) {
-        return a[1] < b[1];
-});
-    //findClosetPairs(pointNum, points);
+    findClosestPairs(points);
 }
